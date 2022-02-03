3bcecb7fa398c8aba46e9a08b79635ee1f99c9df
ReneMuala <renemuala@icloud.com>
2022-02-04 00:07:54 +0200

    Initial Commit

---
+++ Randomic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Randomic
+//
+//  Created by René Descartes Domingos Muala on 04/02/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
